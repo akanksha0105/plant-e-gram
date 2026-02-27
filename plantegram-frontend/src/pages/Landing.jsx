1df5391dad9438887a447d6e37a1398c54662156
Akanksha <mittalakanksha0105@gmail.com>
2026-02-27 22:35:21 +0530

    Landing Page

---
+++ plantegram-frontend/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import Hero from "../features/landing/components/Hero";
+import Navbar from "../shared/components/Navbar";
+
+const Landing = () => {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+		</>
+	);
+};
+
+export default Landing;
