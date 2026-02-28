06e523d9131e750679853ad8b606b5dd3c77cac4
Akanksha <mittalakanksha0105@gmail.com>
2026-02-28 22:46:55 +0530

    Database schema model for user

--- plantegram-backend/src/models/User.js
+++ plantegram-backend/src/models/User.js
@@ -1,15 +1,36 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	username: {
-		type: String,
-		required: true,
-		unique: true,
+	profile: {
+		name: {
+			type: String,
+			required: true,
+		},
+		// username: {
+		// 	type: String,
+		// 	required: true,
+		// 	unique: true,
+		// },
+		gender: {
+			type: String,
+			validate(value) {
+				if (!["male", "female", "others"].includes(value))
+					throw new Error("Gender data is not valid");
+			},
+		},
+		photoURL: {
+			type: String,
+		},
+
+		userBio: {
+			type: String,
+		},
+
+		skill: {
+			type: String,
+			default: "beginner",
+		},
 	},
 
 	emailId: {
@@ -23,24 +44,9 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	gender: {
-		type: String,
-		validate(value) {
-			if (!["male", "female", "others"].includes(value))
-				throw new Error("Gender data is not valid");
-		},
-	},
-	photoURL: {
-		type: String,
-	},
 
-	userBio: {
-		type: String,
-	},
-
-	skill: {
-		type: String,
-		default: "beginner",
+	mypLants: {
+		type: Array,
 	},
 });
 
