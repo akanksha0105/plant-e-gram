22c7d1464bfb6c7b5825a62a276799b6b295bb09
Akanksha <mittalakanksha0105@gmail.com>
2026-03-09 12:54:44 +0530

    Main layout of the pages

--- plantegram-frontend/src/layouts/MainLayout.jsx
+++ plantegram-frontend/src/layouts/MainLayout.jsx
@@ -1,13 +1,21 @@
-// layouts/MainLayout.tsx
+import { navigation } from "../utils/constants";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../shared/components/Navbar";
+import Sidebar from "../shared/components/Sidebar";
 
 const MainLayout = () => {
-	return (
-		<div className='min-h-screen flex flex-col'>
-			<Navbar />
+	const [activeState, setActiveState] = useState(navigation[0].label);
 
-			<main className='flex-1 p-6'>
+	return (
+		<div className='h-screen flex'>
+			<Sidebar activeState={activeState} setActiveState={setActiveState} />
+
+			<main className='bg-background flex-1 flex flex-col overflow-hidden py-10 px-10'>
+				<div className='flex items-center justify-between mb-3 px-0.5'>
+					<h3 className='font-rochester text-xl font-bold text-textPrimary m-0'>
+						{activeState}
+					</h3>
+				</div>
 				<Outlet />
 			</main>
 		</div>
