a85757c4a0720af2ad4f783fecddc4f34988e44f
Akanksha <mittalakanksha0105@gmail.com>
2026-03-16 12:31:06 +0530

    updated garden board

--- plantegram-frontend/src/features/garden/pages/GardenBoard.jsx
+++ plantegram-frontend/src/features/garden/pages/GardenBoard.jsx
@@ -1,20 +1,29 @@
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import ImageUploader from "../components/ImageUploader";
 import savedplantsImage from "../../../assets/totalSavedPlants.png";
 
 import SavedPlantsStrip from "../components/SavedPlantsStrip";
 import DoubleCard from "../../../shared/components/DoubleCard";
 import { useNavigate } from "react-router-dom";
+import { getGardenBoard } from "../gardenBoardSlice";
 
 const GardenBoard = () => {
-	const { savedPlantsSubset, totalSavedCount } = useSelector(
-		(state) => state.savedPlants,
-	);
-	const lastIdentifiedPlant = useSelector(
-		(state) => state.plant.lastIdentifiedPlant,
-	);
+	const {
+		lastIdentifiedPlant,
+		savedPlantsSubset,
+		totalSavedPlantsCount,
+		gardenBoardLoading,
+	} = useSelector((state) => state.gardenBoard);
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
+	console.log("data in garden board", lastIdentifiedPlant);
+	useEffect(() => {
+		dispatch(getGardenBoard());
+	}, []);
+
+	if (gardenBoardLoading) return <div>Loading</div>;
 	return (
 		<div className='flex flex-col gap-8 h-full overflow-auto'>
 			<div className='flex lg:flex-row gap-6 items-stretch'>
@@ -37,17 +46,17 @@ const GardenBoard = () => {
 						<DoubleCard
 							image={savedplantsImage}
 							underlyingHeader={"Total Saved Plants"}
-							description={totalSavedCount}
+							description={totalSavedPlantsCount}
 						/>
 					</div>
 				</div>
 			</div>
 
-			{totalSavedCount > 0 && (
+			{totalSavedPlantsCount > 0 && (
 				<div className='w-full'>
 					<SavedPlantsStrip
 						plants={savedPlantsSubset}
-						totalCount={totalSavedCount}
+						totalCount={totalSavedPlantsCount}
 						newestId={lastIdentifiedPlant?._id}
 					/>
 				</div>
